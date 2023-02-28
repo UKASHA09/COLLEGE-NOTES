7958e5f951be00e7d47ee6b3130047dbef4f31a3
UKASHA09 <UKASHA09@users.noreply.github.com>
2023-02-28 20:31:04 +0530

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeTheme() {
+    var body = document.querySelector("body");
+    var header = document.querySelector("header");
+  
+    if (body.classList.contains("alt-theme")) {
+      body.classList.remove("alt-theme");
+      header.classList.remove("alt-theme");
+    } else {
+      body.classList.add("alt-theme");
+      header.classList.add("alt-theme");
+    }
+  }
+  
